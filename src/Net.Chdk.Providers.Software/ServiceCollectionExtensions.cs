71e927b87fc45f99fdea0abc775698a7bfb9e985
Dmitry Shechtman <damnian@gmail.com>
2017-05-11 19:03:51 +0300

    Add solution

---
+++ src/Net.Chdk.Providers.Software/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Net.Chdk.Providers.Software
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddSoftwareHashProvider(this IServiceCollection serviceCollection)
+        {
+            return serviceCollection
+                .AddSingleton<ISoftwareHashProvider, SoftwareHashProvider>();
+        }
+    }
+}
---
+++ src/Net.Chdk.Providers.Software/SoftwareHashProvider.cs
@@ -0,0 +1,36 @@
+﻿using Net.Chdk.Model.Software;
+using Net.Chdk.Providers.Crypto;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Net.Chdk.Providers.Software
+{
+    sealed class SoftwareHashProvider : ISoftwareHashProvider
+    {
+        private IHashProvider HashProvider { get; }
+
+        public SoftwareHashProvider(IHashProvider hashProvider)
+        {
+            HashProvider = hashProvider;
+        }
+
+        public SoftwareHashInfo GetHash(Stream stream, string fileName, string hashName)
+        {
+            return new SoftwareHashInfo
+            {
+                Name = hashName,
+                Values = GetHashValues(stream, fileName, hashName)
+            };
+        }
+
+        private Dictionary<string, string> GetHashValues(Stream stream, string fileName, string hashName)
+        {
+            var key = fileName.ToLowerInvariant();
+            var value = HashProvider.GetHashString(stream, hashName);
+            return new Dictionary<string, string>
+            {
+                { key, value }
+            };
+        }
+    }
+}
