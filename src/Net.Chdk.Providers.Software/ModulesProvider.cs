ddaf81969c15b0b936cd61e7a9fb7ff67019e742
Dmitry Shechtman <damnian@gmail.com>
2017-05-29 23:26:10 +0300

    Fix automerge

--- src/Net.Chdk.Providers.Software/ModulesProvider.cs
+++ src/Net.Chdk.Providers.Software/ModulesProvider.cs
@@ -35,12 +35,6 @@ namespace Net.Chdk.Providers.Software
 
         #endregion
 
-        #region Private Members
-
-        private ILogger Logger { get; }
-
-        #endregion
-
         #region IModulesProvider Members
 
         public string Path => Data.Modules.Path;
