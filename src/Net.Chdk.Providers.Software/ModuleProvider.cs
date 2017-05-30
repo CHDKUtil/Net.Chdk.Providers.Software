b1a445b019af0318d7916a786d627d78795a706f
Dmitry Shechtman <damnian@gmail.com>
2017-05-30 22:33:29 +0300

    Nest ModuleData

--- src/Net.Chdk.Providers.Software/ModuleProvider.cs
+++ src/Net.Chdk.Providers.Software/ModuleProvider.cs
@@ -71,6 +71,14 @@ namespace Net.Chdk.Providers.Software
             public IDictionary<string, ModuleData> Children { get; set; }
         }
 
+        internal sealed class ModuleData
+        {
+            public string Id { get; set; }
+            public string Title { get; set; }
+            public IDictionary<string, ModuleData> Children { get; set; }
+            public string[] Files { get; set; }
+        }
+
         internal sealed class ComponentsData
         {
             public ModulesData Modules { get; set; }
