c1d663bc47016b28b7d75a272d9730bc1cf1c365
Dmitry Shechtman <damnian@gmail.com>
2017-06-03 21:48:40 +0300

    Add GetSources(SoftwareCategoryInfo)

--- src/Net.Chdk.Providers.Software/SourceProvider.cs
+++ src/Net.Chdk.Providers.Software/SourceProvider.cs
@@ -13,6 +13,12 @@ namespace Net.Chdk.Providers.Software
             ProductSourceProviders = productSourceProviders;
         }
 
+        public IEnumerable<KeyValuePair<string, SoftwareSourceInfo>> GetSources(SoftwareCategoryInfo category)
+        {
+            return ProductSourceProviders
+                .SelectMany(p => p.GetSources(category));
+        }
+
         public IEnumerable<KeyValuePair<string, SoftwareSourceInfo>> GetSources(SoftwareProductInfo product)
         {
             return ProductSourceProviders
